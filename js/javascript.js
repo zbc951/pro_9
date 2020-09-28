44dd0413c9e0163834548df3fe5120cbc30e8ad1
lazio@wataidesign.com <lazio@wataidesign.com>
2020-09-28 17:14:36 +0800

    test2

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,64 @@
+//上選單特效+小菜單特效
+$(function(){
+    $("header li:nth-child(1)").click(function(){
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');//各瀏覽器相容性
+        $body.animate({
+            scrollTop: 0
+        },500);
+    })
+    $("header li:nth-child(2)").click(function(){
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');//各瀏覽器相容性
+        $body.animate({
+            scrollTop: 1430
+        },500);
+    })
+    $("header li:nth-child(3)").click(function(){
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');//各瀏覽器相容性
+        $body.animate({
+            scrollTop: 2230
+        },500);
+    })
+    $(".list button").click(function(){
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');//各瀏覽器相容性
+        $body.animate({
+            scrollTop: 2230
+        },500);
+    })
+    $("header li:nth-child(4)").click(function(){
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');//各瀏覽器相容性
+        $body.animate({
+            scrollTop: 3180
+        },500);
+    })
+    $("header li:nth-child(5)").click(function(){
+        var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');//各瀏覽器相容性
+        $body.animate({
+            scrollTop: 3500
+        },500);
+    })
+})
+
+//上選單hover效果
+$(function(){
+    $(window).scroll(function () {
+        var scrollVal = $(this).scrollTop();
+        if(scrollVal < 1200){
+            $("header ul").attr('class','visited1')
+        }
+        if(scrollVal >= 1200){
+            $("header ul").attr('class','visited2')
+        }
+        if(scrollVal >= 2100){
+            $("header ul").attr('class','visited3')
+        }
+        if(scrollVal >= 3000){
+            $("header ul").attr('class','visited4')
+        }
+        if(scrollVal > 3200){
+            $("header ul").attr('class','visited5')
+        }
+    })
+})
 
 //圖片輪播
 $(document).on('ready', function () {
@@ -8,18 +69,87 @@ $(document).on('ready', function () {
 		autoplay: true,
 		autoplaySpeed: 3500,
         arrows: false,
+        vertical: true,
+        verticalSwiping: true,
 	})
 })
 
 //小選單特效
 $(function(){
     $(window).scroll(function () {
-            var scrollVal = $(this).scrollTop();
-            $("span").text(scrollVal);//span顯示當前scrollbar位置
-            if(scrollVal >= 400){
-                    $(".list section").addClass('slide1')
-                    $(".list button").addClass('slide2')
-                    $(".list div").addClass('slide3')
-            }
+        var scrollVal = $(this).scrollTop();
+        if(scrollVal >= 400){
+            $(".list section").addClass('slide1')
+            $(".list button").addClass('slide2')
+            $(".list div").addClass('slide3')
+        }
+    })
+})
+
+//menu特效
+$(function(){
+    $(window).scroll(function () {
+        var scrollVal = $(this).scrollTop();
+        $("span").text(scrollVal);
+        if(scrollVal >= 1600){
+            $('.menu').animate({
+                opacity: 1,
+            },800)
+        }
+    })
+})
+
+//外送特效
+$(function(){
+    $(window).scroll(function () {
+        var scrollVal = $(this).scrollTop();
+        if(scrollVal >= 2700){
+            $('.uber,.foodpanda').addClass('run')
+        }
+    })
+})
+
+//閃光特效+外送特效2
+$(function(){
+    $('.uberbut').hover(function(){
+        $('.uberbut').toggleClass('flash')
+        $('.uber').toggleClass('run2')
+    })
+    $('.foodpandabut').hover(function(){
+        $('.foodpandabut').toggleClass('flash')
+        $('.foodpanda').toggleClass('run2')
+    })
+})
+
+//關於我們
+$(function(){
+    $(window).scroll(function () {
+        var scrollVal = $(this).scrollTop();
+        if(scrollVal >= 1200){
+            $('.text h2:nth-child(1)').animate({
+                opacity: 1,
+            })
+            $('.text h2:nth-child(2)').delay(400).animate({
+                opacity: 1,
+            })
+            $('.text h2:nth-child(3)').delay(700).animate({
+                opacity: 1,
+            })
+            $('.text h2:nth-child(4)').delay(1000).animate({
+                opacity: 1,
+            })
+            $('.text h3').delay(1200).animate({
+                opacity: 1,
+            })
+            $('.a1').delay(1500).animate({
+                opacity: 1,
+            })
+            $('.a2').delay(2000).animate({
+                opacity: 1,
+            })
+            $('.a3').delay(2500).animate({
+                opacity: 1,
+            })
+        }
     })
 })
